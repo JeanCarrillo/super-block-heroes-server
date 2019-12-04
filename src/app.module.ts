2c1f52fe90f3f262a3123807d7f5f2d7540c4a01
Catherine Vincent <catherine.vincent@hotmail.fr>
2019-12-04 15:52:31 +0100

    add migration system for BDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,27 +9,11 @@ import { ClubModule } from './clubs/clubs.module';
 import { CapacityModule } from './capacities/capacities.module';
 import { GameModule } from './games/games.module';
 import { FriendsModule } from './friends/friends.module';
-import { User } from './users/users-entity';
-import { Monster } from './monster/monsters-entity';
-import { Hero } from './heroes/heroes-entity';
-import { Club } from './clubs/clubs-entity';
-import { Capacity } from './capacities/capacities-entity';
-import { Game } from './games/games-entity';
-import { FriendRelation } from './friends/friends-entity';
 import { Connection } from 'typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [User, Monster, Hero, Club, Capacity, Game, FriendRelation],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     UsersModule,
     MonsterModule,
     HeroesModule,
