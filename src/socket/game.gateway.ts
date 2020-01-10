2e8ba99423a78008d33ba4160a0c8d2580e1e88a
Jean Carrillo <jean.carrillo@ido-data.fr>
2020-01-10 12:11:36 +0100

    score sent to all sockets

--- src/socket/game.gateway.ts
+++ src/socket/game.gateway.ts
@@ -160,7 +160,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @ConnectedSocket() client: Socket,
   ): void {
     const roomId = this.playersRoomsIds[client.id];
-    if (event.eventType === 'useCapacity') {
+    if (event.eventType === 'useCapacity' || event.eventType === 'score') {
       this.server.to(roomId).emit('gameEvent', event);
     } else {
       client.to(roomId).broadcast.emit('gameEvent', event);
